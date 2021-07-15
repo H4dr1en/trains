520939a88eaa0527d0992198d7a3a66fa1e7ea91
H4dr1en <h4dr1en@pm.me>
2021-07-15 14:14:12 +0200

    wait for apt to free

--- clearml/automation/aws_auto_scaler.py
+++ clearml/automation/aws_auto_scaler.py
@@ -27,7 +27,10 @@ class AwsAutoScaler(AutoScaler):
 
     startup_bash_script = [
         "#!/bin/bash",
-        "sleep 30",  # Wait for locks to be released
+        """while sudo fuser /var/{lib/{dpkg,apt/lists},cache/apt/archives}/lock >/dev/null 2>&1; do
+   echo Waiting for other instances of apt to complete...
+   sleep 5
+done""",
         "sudo apt-get update",
         "sudo apt-get install -y python3-dev",
         "sudo apt-get install -y python3-pip",
